04a84164aed43699aa90e388bcfb86893b323d73
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-11-03 22:20:41 -0300

    trying to implement Unet

--- resuneta/models/Unet.py
+++ resuneta/models/Unet.py
@@ -44,7 +44,7 @@ class up_block(nn.HybridBlock):
             self.conv3_1 = ConvBlock(channels, 3)
     def hybrid_forward(self, F, x, s):
         # x = self.upsampler(x)
-        x = F.UpSampling(x, scale=2, sample_type='bilinear', num_filter=self.channels)
+        x = F.UpSampling(x, x, scale=2, sample_type='bilinear', num_filter=self.channels)
         x = self.upconv(x)
         x = self.conv1(x)
         x = F.relu(x)
