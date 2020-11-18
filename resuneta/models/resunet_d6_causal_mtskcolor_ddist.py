38cc510bf89db24a525354891ad44a592d4aebbb
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-11-18 12:22:31 -0300

    Trying to solve symbol probelm on forward

--- resuneta/models/resunet_d6_causal_mtskcolor_ddist.py
+++ resuneta/models/resunet_d6_causal_mtskcolor_ddist.py
@@ -232,7 +232,8 @@ class ResUNet_d6(HybridBlock):
                     # wlogits = res_.forward()
                     # wlogits = self.res(out, self.weights)
                     # wout = out.transpose((0, 2, 3, 1)) * self.weights# .copyto(out.ctx)
-                    wout = F.broadcast_mul(out.transpose((0, 2, 3, 1)), self.weights)
+                    # wout = F.broadcast_mul(out.transpose((0, 2, 3, 1)), self.weights)
+                    wout = F.elemwise_mul(out.transpose((0, 2, 3, 1)), self.weights)
                     # # get back to original shape
                     wlogits = wout.transpose((0, 3, 1, 2))
 
