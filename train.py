05f2302487e9f4ffcaeecfbafd2402a38e777cac
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-11-20 15:18:42 -0300

    Fixing cross entropy weights

--- train.py
+++ train.py
@@ -64,14 +64,14 @@ def train_model(args, net, dataloader, devices, summary_writer, patience=10, del
         loss_dist = gluon.loss.L2Loss() #  TODO: Maybe should put weights for distance transform
         loss_color = gluon.loss.L2Loss()
     elif args.loss == 'wce':
-        weights = mx.nd.array(np.array([1.1060, 238.8582, 0]))
+        # weights = mx.nd.array(np.array([1.1060, 238.8582, 0]))
         # weights = weights / mx.nd.norm(weights)
         # weights = mx.nd.array(np.array([0.2, 0.8, 0]))
-        print(f'New weights: {weights}')
-        # weights = mx.nd.array([1.1494, 33.3333, 0.0])
+        weights = mx.nd.array([1.1494, 33.3333, 0.0])
         # print(type(weights))
         # weights = weights.copyto(devices)
         # weights = [1.1494, 33.3333, 0]
+        print(f'New weights: {weights}')
         loss_clss = WeightedSoftmaxCrossEntropyLoss(axis=1,
                                                     from_logits=from_logits,
                                                     sparse_label=False,
